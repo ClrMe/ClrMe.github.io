8f40c688002f2afe992ae4536596d5afb4da47b7
ClrMe <109436387+ClrMe@users.noreply.github.com>
2023-04-07 17:23:28 -0500

    Add files via upload

--- object.js
+++ object.js
@@ -1,12 +1,30 @@
-const slider = document.querySelector('.slider');
-const arrowLeft = document.querySelector('.arrow-left');
-const arrowRight = document.querySelector('.arrow-right');
-const scrollAmount = 300;
+const galleryImage = document.querySelector(".gallery-image img");
+const leftArrow = document.querySelector(".arrow.left");
+const rightArrow = document.querySelector(".arrow.right");
+let currentIndex = 0;
+const images = [
+  "https://via.placeholder.com/800x600.png?text=Image+1",
+  "https://via.placeholder.com/800x600.png?text=Image+2",
+  "https://via.placeholder.com/800x600.png?text=Image+3",
+  "https://via.placeholder.com/800x600.png?text=Image+4",
+];
 
-arrowLeft.addEventListener('click', function() {
-  slider.scrollLeft -= scrollAmount;
+// Set initial image
+galleryImage.src = images[currentIndex];
+
+// Add click event listeners to arrows
+leftArrow.addEventListener("click", () => {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  galleryImage.src = images[currentIndex];
 });
 
-arrowRight.addEventListener('click', function() {
-  slider.scrollLeft += scrollAmount;
+rightArrow.addEventListener("click", () => {
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  galleryImage.src = images[currentIndex];
 });
